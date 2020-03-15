6e47f53b77d260ba8b28d8691f2c851479d5a737
RmnMatusevich <47831494+RmnMatusevich@users.noreply.github.com>
2020-03-15 14:04:23 +0300

    Refactor functional component to a class component

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,33 +1,46 @@
 import React from "react";
 import NavigationItem from "./NavigationItem";
 import Logo from "./Logo";
-function Navigation() {
-  const props = {
-    text: ["Projects", "Guides", "Blog", "Training and Certification"]
-  };
 
-  return (
-    <nav>
-      <Logo />
-      <div className="mob-menu">
-        <a href="#top" className="menu-button">
-          <span></span>
-        </a>
-        <ul className="mob-navigation">
-          {props.text.map(i => {
-            return <NavigationItem text={i} key={props.text.indexOf(i)} />;
-          })}
-        </ul>
-      </div>
-      <div className="menu">
-        <ul className="navigation">
-          {props.text.map(i => {
-            return <NavigationItem text={i} key={props.text.indexOf(i)} />;
-          })}
-        </ul>
-      </div>
-    </nav>
-  );
+class Navigation extends React.Component {
+  openHide() {
+    const navigation = document.getElementsByClassName("mob-navigation")[0];
+
+    navigation.classList.toggle("navigation__active");
+    if (navigation.style.display === "flex") {
+      navigation.style.display = "none";
+      navigation.style.top = "-100%";
+    } else {
+      navigation.style.display = "flex";
+      navigation.style.top = "0";
+    }
+  }
+  render() {
+    const props = {
+      obj: ["Projects", "Guides", "Blog", "Training and Certification"]
+    };
+    return (
+      <nav>
+        <Logo />
+        <div className="mob-menu">
+          <a href="#top" className="menu-button" onClick={this.openHide}>
+            <span></span>
+          </a>
+          <ul className="mob-navigation">
+            {props.obj.map(i => {
+              return <NavigationItem text={i} key={props.obj.indexOf(i)} />;
+            })}
+          </ul>
+        </div>
+        <div className="menu">
+          <ul className="navigation">
+            {props.obj.map(i => {
+              return <NavigationItem text={i} key={props.obj.indexOf(i)} />;
+            })}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
-
 export default Navigation;
