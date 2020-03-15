6408c46d195c500e4b4e551b451a41dd6fc4177f
RmnMatusevich <47831494+RmnMatusevich@users.noreply.github.com>
2020-03-15 14:05:15 +0300

    Render Project block on the page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Navigation from "./components/Navigation";
+import Projects from "./components/Projects";
+
 ReactDOM.render(<Navigation />, document.getElementById("header"));
+ReactDOM.render(<Projects />, document.getElementById("main"));
