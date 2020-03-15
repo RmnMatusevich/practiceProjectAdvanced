caae43694e42b65103a01dada3d8b0da756f37b4
RmnMatusevich <47831494+RmnMatusevich@users.noreply.github.com>
2020-03-16 02:05:58 +0300

    Refactor functional components to class

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,23 +1,24 @@
 import React from "react";
 import Collage from "./Collage";
 
-function Projects() {
-  return (
-    <section className="main-projects">
-      <h3 className="main-projects__title">Main Projects</h3>
-      <p className="main-projects__description">
-        From configuration to security, web apps to big data—whatever the
-        infrastructure needs of your application may be, there is a{" "}
-        <b>Spring Project</b> to help you build it. Start small and use just
-        what you need—<b>Spring is modular by design.</b>
-      </p>
-      <form id="search">
-        <label id="search-label">Search</label>
-        <input type="search" id="search-input" autoComplete="false" />
-      </form>
-      <Collage />
-    </section>
-  );
+class Projects extends React.Component {
+  render() {
+    return (
+      <section className="main-projects">
+        <h3 className="main-projects__title">Main Projects</h3>
+        <p className="main-projects__description">
+          From configuration to security, web apps to big data—whatever the
+          infrastructure needs of your application may be, there is a{" "}
+          <b>Spring Project</b> to help you build it. Start small and use just
+          what you need—<b>Spring is modular by design.</b>
+        </p>
+        <form id="search">
+          <label id="search-label">Search</label>
+          <input type="search" id="search-input" onInput={Collage.search} />
+        </form>
+        <Collage />
+      </section>
+    );
+  }
 }
-
 export default Projects;
