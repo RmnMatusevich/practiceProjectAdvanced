b891071bdeb0a91d131bf92b9ea8ee35d84257b9
RmnMatusevich <47831494+RmnMatusevich@users.noreply.github.com>
2020-03-29 17:27:54 +0300

    Refactor structure of app
    Move code from index.js to other file

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Projects from "./components/Projects";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route path="/" component={Login}></Route>
+        <Route path="/projects" component={Projects}></Route>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import Projects from "./components/Projects";
-import Login from "./components/Login";
-const routing = (
-  <Router>
-    <div>
-      <Route path="/" component={Login}></Route>
-      <Route path="/projects" component={Projects}></Route>
-    </div>
-  </Router>
-);
+import App from "./App";
 
-ReactDOM.render(routing, document.getElementById("main"));
+ReactDOM.render(App(), document.getElementById("root"));
